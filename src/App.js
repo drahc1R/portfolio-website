c819a773906ee3b555a317a40d89cb28a07c86fd
Richard Bann <richard@bann.com>
2022-07-19 17:57:39 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  RichardBann,
+  Header,
+} from './containers';
+
+import { CTA, Brand, Navbar } from './components';
+
 const App = () => {
-  return <div>Richard Bann</div>;
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <RichardBann />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
