1a8111b27550f640f52630ffc81fd067b398afa1
Richard Bann <richard@bann.com>
2022-07-20 02:25:34 -0300

    pro

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -21,23 +21,22 @@ const Blog = () => (
       <div className="gpt3__blog-container_groupB">
         <Article
           imgUrl={blog02}
-          date="Sep 26, 2021"
-          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          date="December 2021"
+          text="Multi-role Discord Bot"
+          description="A Discord bot that can be used to manage multiple roles on a server."
         />
-        <Article
-          imgUrl={blog03}
-          date="Sep 26, 2021"
-          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
-        />
+        <Article imgUrl={blog03} date="September 2021" text="Dig It!" />
         <Article
           imgUrl={blog04}
-          date="Sep 26, 2021"
-          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          date="June 2022"
+          text="NLP Sentiment Analysis Reddit Bot"
+          description="A Reddit bot that can be used to analyze the sentiment of a post."
         />
         <Article
           imgUrl={blog05}
-          date="Sep 26, 2021"
-          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          date="July 2022"
+          text="The Friday Food E-commerce Website"
+          description="A website that can be used to sell food."
         />
       </div>
     </div>
