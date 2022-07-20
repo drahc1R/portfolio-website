c79f5691312c21aef4e682575fdd345232efb4a3
Richard Bann <richard@bann.com>
2022-07-20 18:27:18 -0300

    puuuush

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -8,7 +8,7 @@ const Possibility = () => (
       <img src={possibilityImage} alt="possibility" />
     </div> */}
     <div className="gpt3__possibility-content">
-      <h4>Request Early Access to Get Started</h4>
+      <h4>Under Construction.</h4>
     </div>
   </div>
 );
